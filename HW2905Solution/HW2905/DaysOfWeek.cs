d4f1f9862edd40e4bc90e7cc43bfb68d6b30c4a3
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-05-29 18:29:14 +0300

    Add files via upload

---
+++ HW2905Solution/HW2905/DaysOfWeek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2905
+{
+    [Flags]
+    enum DaysOfWeek
+    {
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 4,
+        Wednesday = 8,
+        Thursday = 16,
+        Friday = 32,
+        Saturday = 64,
+    }
+}
---
+++ HW2905Solution/HW2905/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2905
+{
+    class Program
+    {
+        private static bool DayAppears(DaysOfWeek listOfDays, DaysOfWeek singleDay)
+        {
+            return listOfDays.HasFlag(singleDay);
+        }
+
+        private static DaysOfWeek AddDay(DaysOfWeek listOfDays, DaysOfWeek singleDay)
+        {
+            return listOfDays | singleDay;
+        }
+
+        private static DaysOfWeek RemoveDay(ref DaysOfWeek listOfDays, DaysOfWeek singleDay)
+        {
+            return listOfDays &~ singleDay;
+        }
+
+        private static void PrintDaysWhichDoesNotAppear(DaysOfWeek listOfDays)
+        {
+            //Array listOfEnums = Enum.GetValues(typeof(DaysOfWeek));
+
+            //Type type = typeof(DaysOfWeek);
+
+            foreach (DaysOfWeek day in Enum.GetValues(typeof(DaysOfWeek)))
+            {
+                if (!listOfDays.HasFlag(day))
+                {
+                    Console.WriteLine(day);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DaysOfWeek dotNetCourseDays = DaysOfWeek.Sunday | DaysOfWeek.Wednesday;
+
+            if (DayAppears(dotNetCourseDays, DaysOfWeek.Thursday))
+            {
+                Console.WriteLine($"dotNetCourseDays contains {DaysOfWeek.Thursday}");
+            }
+            else
+            {
+                Console.WriteLine($"dotNetCourseDays DOES NOT contains {DaysOfWeek.Thursday}");
+            }
+
+            dotNetCourseDays = AddDay(dotNetCourseDays, DaysOfWeek.Thursday);
+            if (DayAppears(dotNetCourseDays, DaysOfWeek.Thursday))
+            {
+                Console.WriteLine($"dotNetCourseDays contains {DaysOfWeek.Thursday}");
+            }
+
+            PrintDaysWhichDoesNotAppear(dotNetCourseDays);
+        }
+    }
+}
